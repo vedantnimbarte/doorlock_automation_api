fecb4ead2516ddccf51b91ab081268cc6fae1652
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-08-06 14:14:42 +0530

    Added themes constants settings to app

---
+++ src/constants/theme.js
@@ -0,0 +1,22 @@
+import {Dimensions} from 'react-native';
+
+const {height, width, scale} = Dimensions.get('window');
+
+export const SIZES = {
+  Height: height,
+  Width: width,
+  Scale: scale,
+};
+
+export const IMAGES = {
+  Logo: require('../assets/images/logo.png'),
+  Plug: require('../assets/images/plug-icon.png'),
+  DoorLock: require('../assets/images/door-lock-icon.png'),
+  BedRoom: require('../assets/images/bedroom.png'),
+};
+
+export const COLORS = {
+  Primary: '#FFC619',
+  Background: '#000000',
+  White: '#ffffff',
+};
