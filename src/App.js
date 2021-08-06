18c3abe659aebd265f2d55ad026727d6741ff433
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-08-06 15:42:21 +0530

    Added stack navigator to the app navigation container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import StackNavigator from './navigation/navigation';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Text>App.js</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
