be58cc30e0e3b21f8c83cb68ee662a3cb4452bd9
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-08-09 15:30:50 +0530

    Added Logout screen to the screens index:

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,6 @@
 import Login from './Login';
 import Home from './Home';
 import SplashScreen from './SplashScreen';
+import Logout from './Logout';
 
-export {Login, Home, SplashScreen};
+export {Login, Home, SplashScreen, Logout};
